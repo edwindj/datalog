8b4a647393bbf05fcb913b7453e8b8389c570043
Edwin de Jonge <edwindjonge@gmail.com>
2014-03-14 17:09:55 +0100

    improved layout

--- R/data_layout.R
+++ R/data_layout.R
@@ -3,7 +3,16 @@
 #'this typically creates
 #' @export 
 data_layout <- function(level, ..., msg=NULL){
-  list(data=list(...), level=level, msg=msg)
+  nms1 <- as.character(as.list(substitute(list(...)))[-1])
+  data <- list(...)
+  nms <- names(data)
+  if (is.null(nms)){
+    nms <- nms1
+  } else {
+    nms[nms==""] <- nms1[nms==""]
+  }
+  names(data) <- nms
+  list(data=data, level=level, msg=msg)
 }
 
 #data_layout("INFO", a=3)
